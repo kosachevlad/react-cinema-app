dd0eaada4236ef2843c455641dae958522bb2ad4
vladyslav.kosachev <vladyslav.kosachev@oracle.com>
2023-03-19 20:09:32 +0200

    vebWitals fixed

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,12 @@
-import webVitals from 'web-vitals';
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && typeof webVitals === 'object') {
-    const { getCLS, getFID, getFCP, getLCP, getTTFB } = webVitals;
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+  if (onPerfEntry) {
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
