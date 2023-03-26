5ed330a32357d477ff02257c4d093a61e7ec781d
vladyslav.kosachev <vladyslav.kosachev@oracle.com>
2023-03-26 20:49:52 +0300

    fix to make clean build

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,4 @@
-import { zonCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry) {
